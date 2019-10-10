f76a838556c3fcd04f3d8ae4610dea95d5ea6747
Piotr Podusowski <podusowski@gmail.com>
2019-10-10 21:56:24 +0200

    urility for setting thread sched

--- src/utils.hpp
+++ src/utils.hpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <ostream>
 #include <iomanip>
+#include <pthread.h>
 
 struct current_time
 {
@@ -13,4 +14,22 @@ std::ostream& operator<<(std::ostream& os, current_time)
     using clock = std::chrono::system_clock;
     std::time_t now_c = clock::to_time_t(clock::now());
     return os << std::put_time(std::localtime(&now_c), "%F %T");
+}
+
+enum class thread_priority
+{
+    low,
+    high
+};
+
+void set_this_thread_scheduling(thread_priority priority)
+{
+    int sched = priority == thread_priority::low ? 50 : 20;
+    int prio = priority == thread_priority::low ? SCHED_OTHER : SCHED_FIFO;
+
+    ::sched_param param;
+    param.sched_priority = prio;
+    int ret = pthread_setschedparam(pthread_self(), sched, &param);
+    if (ret)
+        throw std::runtime_error("failed to set thread scheduling");
 }
