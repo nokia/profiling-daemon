4400494b682c0aedbf368dd3c7fbe520a6f68c63
Piotr Podusowski <podusowski@gmail.com>
2019-11-21 12:10:30 +0100

    Revert "some fixes for cxxopts"
    This reverts commit 78c3abd727d88841e88f5443b3b69db1d6daaa9f.

--- src/options.hpp
+++ src/options.hpp
@@ -10,8 +10,7 @@ namespace poor_perf
 enum class mode_t
 {
     watchdog,
-    oneshot,
-    help
+    oneshot
 };
 
 std::istream& operator>>(std::istream& is, mode_t& mode)
@@ -23,8 +22,6 @@ std::istream& operator>>(std::istream& is, mode_t& mode)
         mode = mode_t::watchdog;
     else if (s == "oneshot")
         mode = mode_t::oneshot;
-    else if (s == "help")
-        mode = mode_t::help;
     else
         is.setstate(std::ios_base::failbit);
 
@@ -36,42 +33,34 @@ std::ostream& operator<<(std::ostream& os, const mode_t&)
     return os << "mode";
 }
 
-/**
- * A wrapper for cxxopts library.
- *
- * Need this since cxxopts's result gets invalidated when `cxxopts::Options` object gets destroyed
- */
 struct options_t
 {
-    options_t(int argc, char **argv) : _desc{"profd", "profiling daemon"}
+    options_t(const cxxopts::ParseResult& opts)
+        : mode{opts["mode"].as<mode_t>()},
+        output{opts["output"].as<std::string>()},
+        duration{opts["duration"].as<std::size_t>()},
+        cpu{opts["cpu"].as<std::size_t>()}
     {
-        _desc.add_options()
-            ("help", "show help")
-            ("output", "where to write profile to; use - for standard output", cxxopts::value<std::string>()->default_value("/rom/profile.txt"))
-            ("cpu", "cpu core to profile on", cxxopts::value<std::size_t>()->default_value("0"))
-            ("duration", "duration in seconds", cxxopts::value<std::size_t>()->default_value("5"))
-            ("mode", "either watchdog or oneshot", cxxopts::value<mode_t>()->default_value("watchdog"));
-
-        auto result = _desc.parse(argc, argv);
-
-        mode = result.count("help") ? mode_t::help : result["mode"].as<mode_t>();
-        output = result["output"].as<std::string>();
-        duration = result["duration"].as<std::size_t>();
-        cpu = result["cpu"].as<std::size_t>();
-    }
-
-    void show_help() const
-    {
-        std::cout << _desc.help() << std::endl;
     }
 
     mode_t mode;
     std::string output;
     std::size_t duration;
     std::size_t cpu;
-
-private:
-    cxxopts::Options _desc;
 };
+
+auto parse_options(int argc, char **argv)
+{
+    cxxopts::Options desc{"profd", "profiling daemon"};
+
+    desc.add_options()
+        ("output", "where to write profile to; use - for standard output", cxxopts::value<std::string>()->default_value("/rom/profile.txt"))
+        ("cpu", "", cxxopts::value<std::size_t>()->default_value("0"))
+        ("duration", "", cxxopts::value<std::size_t>()->default_value("5"))
+        ("mode", "either watchdog or oneshot", cxxopts::value<mode_t>()->default_value("watchdog"));
+
+    // need this `options` wrapper since cxxopts's result gets invalidated when `cxxopts::Options` object gets destroyed
+    return options_t{desc.parse(argc, argv)};
+}
 
 } // namespace
